f3c1ff1cbeafeb49c375a3c9e9f9255e35c6458e
Volodymyr Popovychenko <popovychenko.volodymyr1124@cdu.edu.ua>
2024-11-13 16:49:09 +0200

    Додано підрахунок суми чисел і виведення результату

--- mt/Program.cs
+++ mt/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 5;
+        int y = 10;
+        Console.WriteLine("5+10 = {0}", x + y);
+        Console.ReadLine();
+    }
+}
